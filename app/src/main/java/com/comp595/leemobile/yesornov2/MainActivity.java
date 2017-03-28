b3bbf18e402a0afe93a0a1860b89674f7180a558
Clyde Lee <clyde.lee.51@my.csun.edu>
2017-03-27 23:36:47 -0700

    no message

--- app/src/main/java/com/comp595/leemobile/yesornov2/MainActivity.java
+++ app/src/main/java/com/comp595/leemobile/yesornov2/MainActivity.java
@@ -2,14 +2,29 @@ package com.comp595.leemobile.yesornov2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String myTag="MainActivity";
+
     // Used to load the 'native-lib' library on application startup.
     static {
+        if(OpenCVLoader.initDebug())
+        {
+            Log.d(myTag, "OpenCV successfully loaded!  woot woot.");
+        }
+        else
+        {
+            Log.d(myTag, "OpenCV has not been loaded!");
+        }
+
+
+
         System.loadLibrary("native-lib");
     }
 
@@ -17,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
 
         Mat myMat;
 
